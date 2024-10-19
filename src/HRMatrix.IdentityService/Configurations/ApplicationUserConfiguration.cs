d2b9edbec5106afeb0297fd4d4170224dfff2dea
NicSh <Nicolay.Shmelev@timelysoft.net>
2024-10-19 17:48:48 +0600

    Added info to AspnetUsers(FirstName, LastName, DOB)

--- src/HRMatrix.IdentityService/Configurations/ApplicationUserConfiguration.cs
+++ src/HRMatrix.IdentityService/Configurations/ApplicationUserConfiguration.cs
@@ -20,7 +20,10 @@ public class ApplicationUserConfiguration : IEntityTypeConfiguration<Application
             NormalizedEmail = "USER@MAIL.COM",
             UserName = "user",
             SecurityStamp = Guid.NewGuid().ToString("D"),
-            NormalizedUserName = "USER"
+            NormalizedUserName = "USER",
+            FirstName = "John",
+            LastName = "Doe",
+            DateOfBirth = new DateTime(1990, 1, 1)
         };
         user.PasswordHash = ph.HashPassword(user, "User@123");
 
@@ -31,7 +34,10 @@ public class ApplicationUserConfiguration : IEntityTypeConfiguration<Application
             NormalizedEmail = "SUPERUSER@MAIL.COM",
             UserName = "superuser",
             SecurityStamp = Guid.NewGuid().ToString("D"),
-            NormalizedUserName = "SUPERUSER"
+            NormalizedUserName = "SUPERUSER",
+            FirstName = "Jane",
+            LastName = "Doe",
+            DateOfBirth = new DateTime(1985, 1, 1)
         };
         superUser.PasswordHash = ph.HashPassword(superUser, "SuperUser@123");
 
@@ -42,7 +48,10 @@ public class ApplicationUserConfiguration : IEntityTypeConfiguration<Application
             NormalizedEmail = "HR@MAIL.COM",
             UserName = "hr",
             SecurityStamp = Guid.NewGuid().ToString("D"),
-            NormalizedUserName = "HR"
+            NormalizedUserName = "HR",
+            FirstName = "Alice",
+            LastName = "Smith",
+            DateOfBirth = new DateTime(1992, 1, 1)
         };
         hr.PasswordHash = ph.HashPassword(hr, "Hr@123");
 
@@ -53,7 +62,10 @@ public class ApplicationUserConfiguration : IEntityTypeConfiguration<Application
             NormalizedEmail = "ADMIN@MAIL.COM",
             UserName = "admin",
             SecurityStamp = Guid.NewGuid().ToString("D"),
-            NormalizedUserName = "ADMIN"
+            NormalizedUserName = "ADMIN",
+            FirstName = "Bob",
+            LastName = "Johnson",
+            DateOfBirth = new DateTime(1980, 1, 1)
         };
         admin.PasswordHash = ph.HashPassword(admin, "Admin@123");
 
--- src/HRMatrix.IdentityService/Models/ApplicationUser.cs
+++ src/HRMatrix.IdentityService/Models/ApplicationUser.cs
@@ -4,4 +4,7 @@ namespace HRMatrix.IdentityService.Models;
 
 public class ApplicationUser : IdentityUser<int>
 {
+    public string? FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
 }
